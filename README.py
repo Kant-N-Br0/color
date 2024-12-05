7c9c7e0abc03cc675e89315c977bd73542d9e80b
KantaphitC <465415241001-st@rmutsb.ac.th>
2024-12-05 22:59:15 +0700

    Update README.py

--- README.py
+++ README.py
@@ -1,4 +1,8 @@
-# color
-Kant = Kuy
-Pooh = Kuy2
-Print(Kant)
+# ANSI escape codes format: \033[<code>m
+RED = "\033[31m"
+GREEN = "\033[32m"
+BLUE = "\033[34m"
+RESET = "\033[0m"  # ใช้คืนค่ากลับเป็นปกติ
+
+text = f"{GREEN}This is red text{RESET}"
+print(text)
